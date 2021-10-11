7d50bc29d0c2efa0bdafe4eb47962b382d9f4e19
Rodrigo Bruce Galvez <rodrigobrucegalvez@gmail.com>
2021-10-11 17:52:53 -0500

    feat: :sparkles: Add new Stepper component

---
+++ src/components/Stepper/Stepper.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import Divider from '../Divider/Divider';
+import defaultTheme from '../../defaultTheme';
+
+const StyledStepper = styled.div`
+  min-height: 2rem;
+  display: flex;
+  width: 100%;
+  margin: auto;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  > .cui-divider {
+    position: absolute;
+    z-index: 1;
+  }
+  > .step {
+    z-index: 2;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 1rem;
+    border: 1px solid #a0a0a0;
+    &.button {
+      :hover {
+        cursor: pointer;
+      }
+    }
+    &.numbers {
+      width: 2rem;
+      height: 2rem;
+      background: white;
+    }
+    &.lines {
+      width: 100%;
+      margin: 0 0.75rem;
+      height: 0.5rem;
+    }
+    &.dots {
+      height: 0.5rem;
+      width: 0.5rem;
+    }
+    &.seen {
+      background: ${({ theme }) => theme.color.primary.main};
+      color: white;
+    }
+  }
+`;
+
+const Stepper = ({
+  steps,
+  activeStepIndex,
+  variant,
+  className,
+  onStepClick,
+  ...props
+}) => {
+  return (
+    <StyledStepper className={`cui-stepper ${className}`} {...props}>
+      {variant === 'numbers' && <Divider />}
+      {steps
+        .map((step, index) => ({ ...step, index }))
+        .filter(({ hidden }) => !hidden)
+        .map(({ label, index }) => (
+          <div
+            key={index}
+            className={`step ${variant} ${
+              activeStepIndex >= index ? 'seen' : ''
+            }
+              ${onStepClick ? 'button' : ''}
+            `}
+            role="button"
+            tabIndex={0}
+            onKeyDown={() => false}
+            onClick={() => onStepClick(index)}
+          >
+            {variant === 'numbers' &&
+              (activeStepIndex > index ? <span /> : label)}
+          </div>
+        ))}
+    </StyledStepper>
+  );
+};
+
+StyledStepper.defaultProps = { theme: defaultTheme };
+
+Stepper.propTypes = {
+  steps: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  className: PropTypes.string,
+  activeStepIndex: PropTypes.number,
+  onStepClick: PropTypes.func,
+  variant: PropTypes.oneOf(['numbers', 'lines', 'dots']),
+};
+
+Stepper.defaultProps = {
+  className: '',
+  activeStepIndex: -1,
+  onStepClick: null,
+  variant: 'numbers',
+};
+
+export default Stepper;
---
+++ src/components/Stepper/Stepper.md
@@ -0,0 +1,25 @@
+```jsx
+<span
+  style={{
+    display: 'grid',
+    gridTemplateColumns: '2fr 2fr 1fr',
+    gap: '3rem',
+    justifyItems: 'center',
+    alignItems: 'center',
+  }}
+>
+  <Stepper
+    steps={[{ hidden: true }, { label: 1 }, { label: 2 }, { label: 3 }]}
+    variant="numbers"
+    onStepClick={(e) => alert(e)}
+  />
+  <Stepper
+    steps={[{ hidden: true }, { label: 1 }, { label: 2 }, { label: 3 }]}
+    variant="lines"
+  />
+  <Stepper
+    steps={[{ hidden: true }, { label: 1 }, { label: 2 }, { label: 3 }]}
+    variant="dots"
+  />
+</span>
+```
